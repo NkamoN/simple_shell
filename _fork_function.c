1de04530f0279f69d4b967f79dd29634482d1b9d
NkamoN <r.nkamo@gmail.com>
2023-11-17 17:44:05 +0200

    Fork function errors fixed

--- _fork_function.c
+++ _fork_function.c
@@ -1,37 +1,21 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
- * _fork_fun -Function creates a fork
+ * _fork_fun - Function creates the fork
  *@arg: command and values path
- *@av: name of our program
+ *@av: Program name
  *@env: environment
  *@lineptr: command line for the user
-=======
-
-/**
- * _fork_fun -function 4 creating a fork
- *@arg: command, values path
- *@av: name
- *@env: environment
- *@lineptr: command line
->>>>>>> 68e8cf898335383f31220d817c8bd0b0d62504bd
- *@np: process id
- *@c: Checker add new test
+ *@np: Process id
+ *@c: Checker and add new test
  *Return: 0 success
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> 68e8cf898335383f31220d817c8bd0b0d62504bd
 int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c)
 {
 pid_t child;
 int status;
 char *format = "%s: %d: %s: not found\n";
-<<<<<<< HEAD
 
-=======
->>>>>>> 68e8cf898335383f31220d817c8bd0b0d62504bd
 child = fork();
 
 if (child == 0)
@@ -49,16 +33,10 @@ exit(errno);
 else
 {
 wait(&status);
-<<<<<<< HEAD
 
 if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 return (WEXITSTATUS(status));
 }
-	return (0);
-=======
-if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-return (WEXITSTATUS(status));
-}
 return (0);
->>>>>>> 68e8cf898335383f31220d817c8bd0b0d62504bd
 }
+
