aae46f6baed51a577869d8845a24ceb6ff64a6e9
NkamoN <r.nkamo@gmail.com>
2023-11-17 23:08:15 +0200

    Nana final changes

--- _printf.c
+++ _printf.c
@@ -8,24 +8,24 @@
 */
 void print_binary(unsigned int num, size_t *count)
 {
-	char bin_str[32];
-	int len = 0;
-	int i;
+char bin_str[32];
+int len = 0;
+int i;
 
-	while (num > 0)
-	{
-		bin_str[len++] = (num % 2) + '0';
-		num = num / 2;
-	}
+while (num > 0)
+{
+bin_str[len++] = (num % 2) + '0';
+num = num / 2;
+}
 
-	if (len == 0)
-		bin_str[len++] = '0';
+if (len == 0)
+bin_str[len++] = '0';
 
-	for (i = len - 1; i >= 0; i--)
-	{
-		write(1, &bin_str[i], 1);
-		(*count)++;
-	}
+for (i = len - 1; i >= 0; i--)
+{
+write(1, &bin_str[i], 1);
+(*count)++;
+}
 }
 
 /**
@@ -35,46 +35,46 @@ void print_binary(unsigned int num, size_t *count)
 */
 void print_integer(int num, size_t *count)
 {
-	char *num_str;
-	int len = 0;
-	int i;
-	int max_digits;
+char *num_str;
+int len = 0;
+int i;
+int max_digits;
 
-	if (num == 0)
-	{
-		num_str = malloc(2);
-		if (num_str == NULL)
-		{
-			return;
-		}
-		num_str[len++] = '0';
-	}
-	else if (num < 0)
-	{
-		write(1, "-", 1);
-		(*count)++;
-		num = -num;
-	}
-	max_digits = 12;
+if (num == 0)
+{
+num_str = malloc(2);
+if (num_str == NULL)
+{
+return;
+}
+num_str[len++] = '0';
+}
+else if (num < 0)
+{
+write(1, "-", 1);
+(*count)++;
+num = -num;
+}
+max_digits = 12;
 
-	num_str = malloc(max_digits);
-	if (num_str == NULL)
-	{
-		return;
-	}
+num_str = malloc(max_digits);
+if (num_str == NULL)
+{
+return;
+}
 
-	while (num > 0)
-	{
-		num_str[len++] = num % 10 + '0';
-		num = num / 10;
-	}
+while (num > 0)
+{
+num_str[len++] = num % 10 + '0';
+num = num / 10;
+}
 
-	for (i = len - 1; i >= 0; i--)
-	{
-		write(1, &num_str[i], 1);
-		(*count)++;
-	}
-	free(num_str);
+for (i = len - 1; i >= 0; i--)
+{
+write(1, &num_str[i], 1);
+(*count)++;
+}
+free(num_str);
 }
 
 /**
@@ -84,20 +84,20 @@ void print_integer(int num, size_t *count)
  */
 void print_null_or_str(char *s, size_t *count)
 {
-	size_t len = 0;
+size_t len = 0;
 
-	if (s == NULL)
-	{
-		write(1, "(null)", 6);
-		(*count) += 6;
-	}
-	else
-	{
-		while (s[len] != '\0')
-			len++;
-		write(1, s, len);
-		(*count) += len;
-	}
+if (s == NULL)
+{
+write(1, "(null)", 6);
+(*count) += 6;
+}
+else
+{
+while (s[len] != '\0')
+len++;
+write(1, s, len);
+(*count) += len;
+}
 }
 
 /**
@@ -107,9 +107,9 @@ void print_null_or_str(char *s, size_t *count)
  */
 void print_unknown_specifier(char specifier, size_t *count)
 {
-	write(1, "%", 1);
-	write(1, &specifier, 1);
-	(*count) += 2;
+write(1, "%", 1);
+write(1, &specifier, 1);
+(*count) += 2;
 }
 
 /**
@@ -119,28 +119,28 @@ void print_unknown_specifier(char specifier, size_t *count)
  */
 int _printf(const char *format, ...)
 {
-	size_t count = 0;
-	va_list args;
+size_t count = 0;
+va_list args;
 
-	if (format == NULL)
-		return (-1);
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format != '%')
-			write(1, format, 1), count++;
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-				write(1, format, 1), count++;
-			else
-				handle_format_specifier(*format, args, &count);
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+if (format == NULL)
+return (-1);
+va_start(args, format);
+while (*format)
+{
+if (*format != '%')
+write(1, format, 1), count++;
+else
+{
+format++;
+if (*format == '\0')
+break;
+if (*format == '%')
+write(1, format, 1), count++;
+else
+handle_format_specifier(*format, args, &count);
+}
+format++;
+}
+va_end(args);
+return (count);
 }
--- main.c
+++ main.c
@@ -18,8 +18,8 @@ _printf("\n\n ************************************************************\n");
  */
 void display_prompt(void)
 {
-	_printf("#JoshNana$ ");
-	fflush(stdout);
+_printf("#JoshNana$ ");
+fflush(stdout);
 }
 
 /**
@@ -29,46 +29,46 @@ void display_prompt(void)
  */
 void execute_command(char *command_line, char *simple_shell)
 {
-	pid_t pid;
+pid_t pid;
 
-	if (_strcmp(command_line, "exit") == 0)
-	{
-		_printf("Exit...\n");
-		exit(EXIT_SUCCESS);
-	}
-	if (_strcmp(command_line, "exit") == 0)
-	{
-		char **env = environ;
+if (_strcmp(command_line, "exit") == 0)
+{
+_printf("Exit...\n");
+exit(EXIT_SUCCESS);
+}
+if (_strcmp(command_line, "exit") == 0)
+{
+char **env = environ;
 
-		while (*env != NULL)
-		{
-			_printf("%s\n", *env);
-			env++;
-		}
-		return;
-	}
-	pid = fork();
+while (*env != NULL)
+{
+_printf("%s\n", *env);
+env++;
+}
+return;
+}
+pid = fork();
 
-	if (pid == -1)
-		perror("Forking error");
-	else if (pid == 0)
-	{
-		char *args[MAX_ARGS];
-		int i = 0;
-		char *token = strtok(command_line, " ");
+if (pid == -1)
+perror("Forking error");
+else if (pid == 0)
+{
+char *args[MAX_ARGS];
+int i = 0;
+char *token = strtok(command_line, " ");
 
-		while (token != NULL && i < MAX_ARGS - 1)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-		execvp(args[0], args);
-		perror(simple_shell);
-		exit(EXIT_FAILURE);
-	}
-	else
-		waitpid(pid, NULL, 0);
+while (token != NULL && i < MAX_ARGS - 1)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
+execvp(args[0], args);
+perror(simple_shell);
+exit(EXIT_FAILURE);
+}
+else
+waitpid(pid, NULL, 0);
 }
 /**
  * main - main function
@@ -78,22 +78,22 @@ void execute_command(char *command_line, char *simple_shell)
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	char command[MAXLENGTH];
-	welcome();
-	while (1)
-	{
-		display_prompt();
+char command[MAXLENGTH];
+welcome();
+while (1)
+{
+display_prompt();
 
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			_printf("\nExit...\n");
-			break;
-		}
+if (fgets(command, sizeof(command), stdin) == NULL)
+{
+_printf("\nExit...\n");
+break;
+}
 
-		command[strcspn(command, "\n")] = '\0';
+command[strcspn(command, "\n")] = '\0';
 
-		execute_command(command, argv[0]);
-	}
-	return (0);
+execute_command(command, argv[0]);
+}
+return (0);
 }
 
